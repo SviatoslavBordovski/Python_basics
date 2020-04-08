52fc4f755aca07c6f2ffc2f0123f31da4f53bcb8
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-04-08 15:40:38 +0300

    Create if-else_loops.py

---
+++ pybasics/if-else_loops.py
@@ -0,0 +1,36 @@
+if 10 > 3:
+    print('10 is really greater than 3')
+
+num = 0
+if num  > 0:
+    print('Fantastic!')
+elif num == 0:
+    print('Number equals zero')
+else:
+    print('Number is negative')
+
+num = [1,2,3,4,5]
+sum=0
+for var in num:
+    sum=sum+var
+
+print('TOTAL IS', sum)
+
+fruits = ['Five apples', 'Three oranges', 'Ten grapes']
+
+print('Lets count all the fruits from our array basket (-:')
+
+for count in fruits:
+    print(count)
+else:
+    print('You ate all the fruits! ^_^')
+
+num = 10
+val = 0
+x = 1
+
+while x < num:
+    val=val+x
+    x=x+1
+    print(val, x)
+print('Final prompt of "val" number when "x" becomes 10 is: ', val)
