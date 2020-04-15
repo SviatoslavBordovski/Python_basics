f153b37ff9af298bb554bc4732fdfb374c5691d3
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-04-15 20:30:53 +0300

    Create tuple.py

---
+++ Collections/tuple.py
@@ -0,0 +1,29 @@
+# Tuple () ordered | indexed | unchangeable | duplicates
+
+#Tuple manipulations:
+tuple_1 = ('Apples', 'Bananas', 'Grapes')
+print(tuple_1)
+print(tuple_1[1]) #Executed second in the list of tuple_1
+print(tuple_1[-1]) #Executed first from the end
+print(tuple_1[0:2]) #Executed selected data range of the tuple_1
+
+for fruit in tuple_1:
+    print(fruit)
+
+print(len(tuple_1))
+
+#It is time to walk through new tuple and change something there:
+tuple_2 = ('Oranges', (1,5,10), ['Ford', 'Toyota', 'Honda'])
+print(tuple_2)
+print(tuple_2[2][0]) #Executed 'Ford'
+
+tuple_2[2][0] = 'Nissan' #We are replacing Ford in the tuple_2
+print(tuple_2)
+
+print('Bananas' in tuple_1) #Output would give us True
+print('Cherry' in tuple_1) #Output would give us False
+
+#Errors or limitations of tuple:
+tuple_1[3] = 'Cherry' #it is not possible to add / assign new value to tuple
+del tuple_1[2] #impossible to delete particular value from the tuple
+del tuple_1 #however, it is allowed to delete tuple fully
