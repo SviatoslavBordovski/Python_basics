a264e8fe115f34a75ef9d4d5e59027f05ae61920
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-04-27 21:43:15 +0300

    Update tests_cases.py

--- Unit tests/tests_cases.py
+++ Unit tests/tests_cases.py
@@ -7,6 +7,6 @@ from unittest_file import Burito
 
 class MyTestCase(unittest.TestCase):
 
-    def test_add(self):
+    def test_add(self): #Quick test of the values result when they are added, we expect that values would be added and result is calculated
         result = Burito.add(self, 10, 20)
         self.assertEqual(result, 30)
