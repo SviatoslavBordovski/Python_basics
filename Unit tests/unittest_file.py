dbeaae9e2ef90005be26ad8cce47c4e8861793d2
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-06 21:30:24 +0300

    Update unittest_file.py

--- Unit tests/unittest_file.py
+++ Unit tests/unittest_file.py
@@ -5,5 +5,5 @@ class Burito:
     def add(self,x,y): #declare a function that adds two variables
         return x + y
 
-    def sub(self,x,y): #declare a function that subtracts one variable value from another
+    def sub(self,x,y): #declare a function that subtracts one variable value from another one
         return x - y
