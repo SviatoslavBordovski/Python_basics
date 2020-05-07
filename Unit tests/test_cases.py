7bc26201d3d312d1aa8e09eec276a8a88ce4501c
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-07 22:08:12 +0300

    Update test_cases.py

--- Unit tests/test_cases.py
+++ Unit tests/test_cases.py
@@ -29,5 +29,5 @@ class MyTestCase(unittest.TestCase):
         result = Burito.sub(self, 100, 50)
         self.assertEqual(result, 50)
 
-    def test_notSimilar(self):
+    def test_notSimilar(self): #Check values are not the same
         self.assertIsNot(Burito.notSimilar(self, 50, 60), 50)
