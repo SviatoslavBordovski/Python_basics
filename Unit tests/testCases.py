f3d8fd4866abec9a09b5be920b680dd494761379
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-17 21:24:22 +0300

    Update testCases.py

--- Unit tests/testCases.py
+++ Unit tests/testCases.py
@@ -11,8 +11,14 @@ class JustSimpleTestCase(unittest.TestCase):
         self.driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
-        
-        
+       
+    def test_search(self):
+        self.driver.get('http://www.google.com') #Open website
+        self.driver.find_element_by_name('q').send_keys('Python documentation') #Type search keys
+        self.driver.find_element_by_name('btnK').click() #Click on 'Google search' button
+        self.driver.find_element_by_class_name('LC20lb').click() #Navigate to the selected link
+        time.sleep(3) #Verify loaded page
+        print('Python documentation detected!') #Execute test case result in the terminal
         
     def tearDown(self):
         self.driver.close()
