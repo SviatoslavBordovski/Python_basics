12a38ec577df5210bd995774b3dbfe3e09134cca
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-17 21:22:12 +0300

    Create testCases.py

---
+++ Unit tests/testCases.py
@@ -0,0 +1,13 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class JustSimpleTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
