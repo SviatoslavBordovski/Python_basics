b82ba21561674458276c3a10e916de805db7f585
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-17 21:30:41 +0300

    Update testCases.py

--- Unit tests/testCases.py
+++ Unit tests/testCases.py
@@ -15,7 +15,7 @@ class JustSimpleTestCase(unittest.TestCase):
     def test_search(self):
         self.driver.get('http://www.google.com') #Open website
         self.driver.find_element_by_name('q').send_keys('Python documentation') #Type search keys
-        self.driver.find_element_by_name('btnK').click() #Click on 'Google search' button
+        self.driver.find_element_by_name('btnK').click() #Find and click on 'Google search' button
         self.driver.find_element_by_class_name('LC20lb').click() #Navigate to the selected link
         time.sleep(3) #Verify loaded page
         print('Python documentation detected!') #Execute test case result in the terminal
