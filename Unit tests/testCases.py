25cb76eaa00a9e92e272da4a7ae4ca968d522fe2
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-25 22:32:36 +0300

    Update testCases.py

--- Unit tests/testCases.py
+++ Unit tests/testCases.py
@@ -12,13 +12,10 @@ class JustSimpleTestCase(unittest.TestCase):
         cls.driver.implicitly_wait(10) #Wait until element becomes visible for interaction
         cls.driver.maximize_window() #Make browser window opened fully
        
-    def test_search(self):
-        self.driver.get('http://www.google.com') #Open the needed website
-        self.driver.find_element_by_name('q').send_keys('Python documentation') #Type search keys
-        self.driver.find_element_by_name('btnK').click() #Find and click on 'Google search' button
-        self.driver.find_element_by_class_name('LC20lb').click() #Navigate to the selected link
-        time.sleep(3) #Verify loaded page with short pause
-        print('Python documentation detected!') #Execute test case result in the terminal
+    def test_openWebsite(self):
+        self.driver.get('https://bordovski.pp.ua')
+        time.sleep(5)
+        print('Personal blog detected')
         
     @classmethod
     def tearDownClass(cls): #Conditions that run after every test execution
