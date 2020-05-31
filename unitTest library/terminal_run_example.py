cc618ee24c415bad8bb1a25fa19d497c3ccf57c0
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-31 21:46:15 +0300

    Update terminal_run_example.py

--- unitTest library/terminal_run_example.py
+++ unitTest library/terminal_run_example.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest_file import Burito
 
-# Run Python unit tests in this file through Linux terminal to execute the result with the specified bash command:
+# Run Python tests with unittest library in this file through Linux terminal to execute the result with the specified bash command:
 # 'python3 -m unittest test_cases.py' (it confirms usage of 3rd Python version since 2nd version is not supported from 2020)
 
 class MyTestCase(unittest.TestCase):
