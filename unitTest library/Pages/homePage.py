ac365525b1c91f17952220f2d33dc2de86a7d63b
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-07-01 22:40:34 +0300

    Update homePage.py

--- unitTest library/Pages/homePage.py
+++ unitTest library/Pages/homePage.py
@@ -9,5 +9,5 @@ class HomePage():
     def click_welcome(self): #function that makes click for the button with a specified id
         self.driver.find_element_by_id(self.welcome_link_id).click()
         
-    def click_logout(self):
+    def click_logout(self): #function that makes click for the button with a specified link text
         self.driver.find_element_by_link_text(self.logout_link_linkText).click()
