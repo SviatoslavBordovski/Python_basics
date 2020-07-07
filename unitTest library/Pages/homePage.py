454464cffc0343d6e995c37f1f6514d2bec18bd8
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-07-07 23:22:43 +0300

    Update homePage.py

--- unitTest library/Pages/homePage.py
+++ unitTest library/Pages/homePage.py
@@ -1,13 +1,15 @@
+from ohrm.Locators.locators import Locators
+
 class HomePage():
     
     def __init__(self,driver): #function that defines locators to be clicked
         self.driver = driver
         
-        self.welcome_link_id = 'welcome'
-        self.logout_link_linkText = 'Logout'
+        self.welcome_link_id = Locators.welcome_link_id
+        self.logout_link_linkText = Locators.logout_link_linkText
         
     def click_welcome(self): #function that makes click for the button with a specified id
-        self.driver.find_element_by_id(self.welcome_link_id).click()
+        self.driver.find_element_by_id(Locators.welcome_link_id).click()
         
     def click_logout(self): #function that makes click for the button with a specified link text
-        self.driver.find_element_by_link_text(self.logout_link_linkText).click()
+        self.driver.find_element_by_link_text(Locators.logout_link_linkText).click()
