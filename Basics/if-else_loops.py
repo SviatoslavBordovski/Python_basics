061b375eef570356ffcd6521c569479d089a257c
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-07-14 21:55:03 +0300

    Update if-else_loops.py

--- Basics/if-else_loops.py
+++ Basics/if-else_loops.py
@@ -33,7 +33,7 @@ num = 10
 val = 0
 x = 1
 
-while x < num: #Loop that gives opprotunity to to run it until some value achieves the goal
+while x < num: #Loop that gives opportunity to run it until some value will achieve the goal
     val = val + x
     x = x + 1
     print(val, x)
