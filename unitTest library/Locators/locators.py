3f9e6bc3a20dd6b3df2f757d7329fd830e18cc2b
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-07-27 23:45:58 +0300

    Update locators.py

--- unitTest library/Locators/locators.py
+++ unitTest library/Locators/locators.py
@@ -8,3 +8,16 @@ class Locators():
     #Home page locators
     welcome_link_id = 'welcome'
     logout_link_linkText = 'Logout'
+    
+    #Add new user locators
+    view_users_button_id = 'menu_admin_viewAdminModule'
+    add_user_button_id = 'btnAdd'
+    userRole_dropdown_id = 'systemUser_userType'
+    userRole_option_xpath_admin = '//*[@id="systemUser_userType"]/option[1]'
+    employeeName_textbox_id = 'systemUser_employeeName_empName'
+    new_username_textbox_id = 'systemUser_userName'
+    userStatus_dropdown_id = 'systemUser_status'
+    userStatus_option_xpath_enabled = '//*[@id="systemUser_status"]/option[1]'
+    new_user_password_textbox_id = 'systemUser_password'
+    confirm_password_textbox_id = 'systemUser_confirmPassword'
+    save_newUser_button_id = 'btnSave'
