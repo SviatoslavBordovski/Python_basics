447ce0c9b0de42520c19d052850d7401e7703502
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-07-27 23:44:26 +0300

    Create createUser_positiveTest.py

---
+++ unitTest library/Tests/createUser_positiveTest.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+import unittest
+import HtmlTestRunner
+import time
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from ohrm.Pages.loginPage import LoginPage
+from ohrm.Pages.homePage import HomePage
+from ohrm.Pages.createUserPage import CreateUser
+#from ohrm.Tests.signIn_signOut_positiveTest import positiveSignIn
+#from ohrm.Tests.signIn_signOut_negativeTest import negativeSignIn
+from selenium.webdriver.common.keys import Keys
+
+class userManagement(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
+        cls.driver.implicitly_wait(5)
+        cls.driver.maximize_window()        
+        
+    def test_3_createValidUser(self):
+        driver = self.driver
+        driver.get('https://opensource-demo.orangehrmlive.com/')
+        
+        newUser = LoginPage(driver)
+        newUser.enter_username('admin')
+        newUser.enter_password('admin123')
+        newUser.click_login()
+        
+        newUser = CreateUser(driver)
+        newUser.adminSection_click()
+        newUser.addUser_click()
+        newUser.userRole()
+        newUser.newEmployeeName('Hello world')
+        newUser.newUsername('Superuser26')
+        newUser.status_enabled()
+        newUser.new_user_password('Superuser2020!')
+        newUser.new_user_confirm_password('Superuser2020!')
+        newUser.click_save_user()
+        
+        # driver.find_element_by_id('menu_admin_viewAdminModule').click()
+        # driver.find_element_by_id('btnAdd').click()
+        # driver.find_element_by_id('systemUser_userType').click()
+        # driver.find_element_by_xpath('//*[@id="systemUser_userType"]/option[1]').click()
+        # driver.find_element_by_id('systemUser_employeeName_empName').send_keys('Hello world')
+        # driver.find_element_by_id('systemUser_userName').send_keys('Superuser25')
+        # driver.find_element_by_id('systemUser_status').click()
+        # driver.find_element_by_xpath('//*[@id="systemUser_status"]/option[1]').click()
+        # driver.find_element_by_id('systemUser_password').send_keys('Superuser2020!')
+        # driver.find_element_by_id('systemUser_confirmPassword').send_keys('Superuser2020!')
+        # driver.find_element_by_id('btnSave').click()
+        time.sleep(10)
+        
+        confirmationLink = 'https://opensource-demo.orangehrmlive.com/index.php/admin/viewSystemUsers'
+        
+        if driver.current_url == confirmationLink:
+            print(confirmationLink + ' / New user was created successfully!')
+        else:
+            #return error('User not created')
+            self.fail('Refactor me')
+            
+        newUser = HomePage(driver)
+        newUser.click_welcome()
+        newUser.click_logout()
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+if __name__== '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='/home/incognito/Downloads/everything/projects/reports'))
