d6b4209419dd1c14e04b543433c42adca78e78f7
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-08-06 23:17:30 +0300

    Update createUser_positiveTest.py

--- unitTest library/Tests/createUser_positiveTest.py
+++ unitTest library/Tests/createUser_positiveTest.py
@@ -52,7 +52,7 @@ class userManagement(unittest.TestCase):
         else:
             self.fail('Refactor me')
         
-        #Logout from HRM
+        #Logout from OrangeHRM
         newUser = HomePage(driver)
         newUser.click_welcome()
         newUser.click_logout()
