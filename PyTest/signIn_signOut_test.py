f619415cce17e0aea6505bf601f66a4e7293d78e
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-08-24 22:30:43 +0300

    Create signIn_signOut_test.py

---
+++ PyTest/signIn_signOut_test.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import sys
+import os
+import pytest
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from selenium.webdriver.common.keys import Keys
+from lib2to3.pgen2 import driver
+
+@pytest.fixture()
+def test_setup():
+    global driver
+    driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    yield
+    driver.close()
+    driver.quit()
+    print('Mission accomplished')
+
+def test_validLoginLogout(test_setup):
+    driver.get('https://opensource-demo.orangehrmlive.com/')
+    driver.find_element_by_id('txtUsername').send_keys('Admin')
+    driver.find_element_by_id('txtPassword').send_keys('admin123')
+    driver.find_element_by_id('btnLogin').click()
+    time.sleep(3)
+    driver.find_element_by_id('welcome').click()
+    driver.find_element_by_link_text('Logout').click()
+    time.sleep(3)
+    x = driver.title
+    assert x == 'OrangeHRM'
+    print('Passed Test')
+
+# def test_tearDown():
+#     driver.close()
+#     driver.quit()
+#     print('Mission accomplished')
