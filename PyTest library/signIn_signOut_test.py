a6fa44e6236d856d8c40b7ad4539104f72690e68
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-08-24 22:40:33 +0300

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -11,7 +11,7 @@ from lib2to3.pgen2 import driver #snippet that resolves the errors in vs code
 def test_setup():
     global driver
     driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
     driver.maximize_window()
     yield
     driver.close()
