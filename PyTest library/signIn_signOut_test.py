2ebea81a2e68ffbbf06524cb4eb1f7bb79cecd39
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-08-28 23:55:52 +0300

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -27,5 +27,5 @@ def test_validLoginLogout(test_setup):
     driver.find_element_by_id('welcome').click()
     driver.find_element_by_link_text('Logout').click()
     x = driver.title
-    assert x == 'OrangeHRM' #check for the True value of the page title
+    assert x == 'OrangeHRM' #check for the True value of the page title read from the HTML
     print('Mission accomplished!')
