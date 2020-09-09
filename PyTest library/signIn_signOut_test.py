e7bd03b3dc43da89157730c9100a8504356f1b83
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-09-09 22:30:34 +0300

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -5,7 +5,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from selenium.webdriver.common.keys import Keys
-from lib2to3.pgen2 import driver #This code snippet resolves importing errors in VS code IDE
+from lib2to3.pgen2 import driver #This code snippet resolves importing errors in VS code IDE, but use PyCharm and see no errors :D
 
 @pytest.fixture() #Less code == more value ^_^
 def test_setup(): #setup and clean up after test finished
