3fba172a3de1df5d5e40765dc914a6d87afbc618
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-10-03 23:12:54 +0300

    Update createUser_positiveTest.py

--- unitTest library/Tests/createUser_positiveTest.py
+++ unitTest library/Tests/createUser_positiveTest.py
@@ -44,7 +44,7 @@ class userManagement(unittest.TestCase):
 
         time.sleep(5) #Time to check a confirmation link during test execution
         
-        #Confirm the link that should be verified after a successful adding the user
+        #Confirmation link that should be verified after a successful adding the user
         confirmationLink = 'https://opensource-demo.orangehrmlive.com/index.php/admin/viewSystemUsers'
         
         if driver.current_url == confirmationLink:
