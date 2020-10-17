5488158793a718b59fbdd2e6905eca8d5f93c756
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-10-17 22:10:20 +0300

    Update signIn_signOut_positiveTest.py

--- unitTest library/Tests/signIn_signOut_positiveTest.py
+++ unitTest library/Tests/signIn_signOut_positiveTest.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.keys import Keys
 class signInTests(unittest.TestCase):
     
     @classmethod
-    def setUpClass(cls): #Chosen browser settings before it's opening
+    def setUpClass(cls): #Chosen browser settings before it's launch
         cls.driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
         cls.driver.implicitly_wait(8) #Wait until searching element would be visible for driver
         cls.driver.maximize_window() #Maximize browser window to avoid issues related to website responsiveness/locator search
