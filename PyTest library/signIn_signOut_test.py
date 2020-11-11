d8212b57569f659a7115f6fb6b2743f82e1d834f
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-11-11 21:51:30 +0200

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -30,7 +30,7 @@ def test_validLoginLogout(test_setup):
     assert x == 'OrangeHRM' #check for 'True' value of the page title which is read from the page HTML
     print('This test should be skipped ;)')
     
-@pytest.mark.skip(reason='test is not in scope of the sprint') #this test would be skipped, reason in brackets could be specified
+@pytest.mark.skip(reason='test is not in scope of the sprint') #this test would be skipped, reason could be specified in brackets
 def test_invalid_login_logout(test_setup):
     driver.get('https://opensource-demo.orangehrmlive.com/')
     driver.find_element_by_id('txtUsername').send_keys('admin')
