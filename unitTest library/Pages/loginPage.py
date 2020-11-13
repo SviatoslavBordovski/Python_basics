09ee8b70e014a158c9452294ffb8186d3306e769
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-11-13 23:28:14 +0200

    Update loginPage.py

--- unitTest library/Pages/loginPage.py
+++ unitTest library/Pages/loginPage.py
@@ -9,11 +9,11 @@ class LoginPage():
         self.password_textbox_id = Locators.password_textbox_id
         self.login_button_id = Locators.login_button_id
         
-    def enter_username(self, username): #function that cleans the field and types username in the field with specified id of the element
+    def enter_username(self, username): #function that cleans field and types username in the field with specified id of the element
         self.driver.find_element_by_id(Locators.username_textbox_id).clear()
         self.driver.find_element_by_id(Locators.username_textbox_id).send_keys(username)
         
-    def enter_password(self, password): #function that cleans the field and types password in the field with specified id of the element
+    def enter_password(self, password): #function that cleans field and types password in the field with specified id of the element
         self.driver.find_element_by_id(Locators.password_textbox_id).clear()
         self.driver.find_element_by_id(Locators.password_textbox_id).send_keys(password)
         
