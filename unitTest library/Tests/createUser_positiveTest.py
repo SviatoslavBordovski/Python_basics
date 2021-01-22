9e2021a7a083b9c0e796136be277e58ef6f8a64e
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-01-22 23:10:55 +0200

    Update createUser_positiveTest.py

--- unitTest library/Tests/createUser_positiveTest.py
+++ unitTest library/Tests/createUser_positiveTest.py
@@ -17,7 +17,7 @@ class userManagement(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
-        cls.driver.implicitly_wait(10)
+        cls.driver.implicitly_wait(5)
         cls.driver.maximize_window()
  
     def test_3_createValidUser(self):
