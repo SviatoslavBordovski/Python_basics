5e44eb5c79951c5a80aba39eaf24430bd4411804
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-02-09 22:12:09 +0200

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -28,7 +28,7 @@ def test_validLoginLogout(test_setup):
     driver.find_element_by_link_text('Logout').click()
     x = driver.title
     assert x == 'OrangeHRM' #check for 'True' value of the page title which is read from the HTML tree
-    print('This test should be skipped!')
+    print('This test is skipped!')
     
 @pytest.mark.skip(reason='test is not in scope of sprint') #test would be skipped, reason could be specified in the brackets
 def test_invalid_login_logout(test_setup):
