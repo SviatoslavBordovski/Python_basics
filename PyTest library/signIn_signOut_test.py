76a07219529f371edbadf4ee8abd9967ccecdfe2
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-03-11 21:18:45 +0200

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -13,7 +13,7 @@ def test_setup(): #setup and clean up the tests
     driver = webdriver.Chrome(executable_path='/home/incognito/Downloads/drivers/chromedriver')
     driver.implicitly_wait(5)
     driver.maximize_window()
-    yield #using an 'yield' statement instead of a 'return' statement, all code would run after a 'yield' statement as a teardown
+    yield #using a 'yield' statement instead of a 'return' statement, all code would run after a 'yield' statement as a teardown
     driver.close()
     driver.quit()
     print('All tests passed!')
