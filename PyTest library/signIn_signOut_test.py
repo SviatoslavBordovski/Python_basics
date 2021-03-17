9985c293350dafacb7b76fbf004f097cd15372a0
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-03-17 21:02:09 +0200

    Update signIn_signOut_test.py

--- PyTest library/signIn_signOut_test.py
+++ PyTest library/signIn_signOut_test.py
@@ -23,7 +23,7 @@ def test_validLoginLogout(test_setup):
     driver.find_element_by_id('txtUsername').send_keys('Admin')
     driver.find_element_by_id('txtPassword').send_keys('admin123')
     driver.find_element_by_id('btnLogin').click()
-    time.sleep(5)
+    time.sleep(10)
     driver.find_element_by_id('welcome').click()
     driver.find_element_by_link_text('Logout').click()
     x = driver.title
@@ -36,10 +36,10 @@ def test_invalid_login_logout(test_setup):
     driver.find_element_by_id('txtUsername').send_keys('admin')
     driver.find_element_by_id('txtPassword').send_keys('admin123')
     driver.find_element_by_id('btnLogin').click()
-    time.sleep(5)
+    time.sleep(10)
     driver.find_element_by_id('welcome').click()
     driver.find_element_by_link_text('Logout').click()
-    time.sleep(5)
+    time.sleep(10)
     x = driver.title
     assert x == 'OrangeHRM'
     print('Login_Logout test passed')
